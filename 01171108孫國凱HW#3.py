5fa5b7e77b676830b3b62a9169409ece7b8d5b8b
skynoel <66463016+skynoel@users.noreply.github.com>
2023-10-13 08:59:07 +0800

    class1

--- 01171108孫國凱HW#3.py
+++ 01171108孫國凱HW#3.py
@@ -1,3 +1,31 @@
 #HomeWork#3 from  01171108孫國凱HW#3.py
 
 print("hi world")
+
+#onclass
+def sayHello():
+    print("Hello")
+def say(msg):
+    print(msg)
+    return "hello"
+def add(n1,n2):
+    result = n1+n2
+    return result
+
+
+sayHello()
+value = say("Hello function")
+print(value)
+say("Hello python")
+value=add(3,4)
+print(value)
+value=add(7,8)
+print(value)
+
+
+
+
+
+
+
+# part1
