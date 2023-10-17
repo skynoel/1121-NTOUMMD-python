12dd2c041267347e2bcae5dda572538b216e0393
skynoel <66463016+skynoel@users.noreply.github.com>
2023-10-18 01:40:20 +0800

    678

--- 01171108孫國凱HW#3.py
+++ 01171108孫國凱HW#3.py
@@ -1,5 +1,7 @@
 #HomeWork#3 from  01171108孫國凱HW#3.py
 # part1
+import time
+
 def add(n1,n2):
     return n1+n2
 def minus(x,y):
@@ -16,7 +18,6 @@ def square(x):
     return x*x
 
 type=int(input("請輸入你想做的運算:1=加,2=減,3=乘,4=除,5=取餘數,6=取整數,7=次方,0=退出："))
-print("\n")
 while(type!=0):
     number1=float(input("請輸入第一個數："))
     if type!=7:
@@ -38,11 +39,18 @@ while(type!=0):
     else:
         print("在幹嘛，不要亂打")
     type=int(input("請輸入你想做的運算:1=加,2=減,3=乘,4=除,5=取餘數,6=取整數,7=次方,0=退出"))
+if type==0:
+    print("成功退出")
 #part 2
+time.sleep(1)
+print("進入第二部分")
+print("輸出乘法表")
+time.sleep(1)
 for i in range(1, 10):
     for j in range(1, 10):
-        print("%d * %d = %d" % (i, j, i*j),end=" ")
-    print("/n")
+        print("%d * %d = %2s" % (i, j, i*j),end="  ")        
+    print("\n")
+
 
 
 
